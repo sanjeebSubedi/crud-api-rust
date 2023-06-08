112945d7b8b063e7be0b07f8e5513acf2ddadee7
Sanjeeb Subedi <spur@pop-os.localdomain>
2023-06-08 14:05:26 +0545

    add routes for login and books_by_user apis

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod database;
 mod handlers;
 mod models;
+mod schemas;
 use axum::{
     routing::{get, post},
     Extension, Router,
 };
 use dotenv::dotenv;
 use handlers::{
-    book::{create_book, get_all_books},
-    user::create_user,
+    book::{create_book, get_all_books, get_books_by_user},
+    user::{create_user, user_login},
 };
 use std::env;
 use std::error::Error;
@@ -23,6 +24,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/books", get(get_all_books))
         .route("/books", post(create_book))
         .route("/users", post(create_user))
+        .route("/users/books", get(get_books_by_user))
+        .route("/login", get(user_login))
         .layer(Extension(db));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
