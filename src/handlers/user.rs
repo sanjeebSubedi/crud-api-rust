5f26929482b3b6fc4cb8e4eaf9db831f0b639d6e
Sanjeeb Subedi <spur@pop-os.localdomain>
2023-06-08 10:19:32 +0545

    password hash, email validation

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,34 +1,43 @@
 use axum::Json;
 use sqlx::PgPool;
+use validator::Validate;
 
-use crate::models::user::{CreateUser, CreateUserResponse};
+use crate::models::user::{CreateUser, CreateUserResponse, CreateUserResponseData};
+use bcrypt::{hash, DEFAULT_COST};
+
+pub fn return_response(status: &str, message: &str) -> Json<CreateUserResponse> {
+    Json(CreateUserResponse {
+        status: status.to_owned(),
+        data: CreateUserResponseData {
+            message: message.to_owned(),
+        },
+    })
+}
 
 pub async fn create_user(
     pool: axum::extract::Extension<PgPool>,
     Json(user): Json<CreateUser>,
 ) -> Json<CreateUserResponse> {
-    let mut conn = pool
-        .acquire()
-        .await
-        .expect("Failed to acquire connection from the pool");
+    if user.validate().is_err() {
+        return return_response("Failed!", "Validation failed!");
+    }
 
-    let query = "INSERT INTO users(username, password, name) VALUES ($1, $2, $3)";
+    let hashed = match hash(user.password, DEFAULT_COST) {
+        Ok(hashed) => hashed,
+        Err(_) => return return_response("Failed", "Failed to hash the password"),
+    };
+
+    let query = "INSERT INTO users(email, password, name) VALUES ($1, $2, $3)";
 
     let res = sqlx::query(query)
-        .bind(&user.username)
-        .bind(&user.password)
+        .bind(&user.email)
+        .bind(hashed)
         .bind(user.name)
-        .execute(&mut conn)
+        .execute(&*pool)
         .await;
-    println!("{:?}", res);
-    match res {
-        Ok(_) => Json(CreateUserResponse {
-            status: "Success".to_owned(),
-            data: "New user was successfully created!".to_owned(),
-        }),
-        Err(_) => Json(CreateUserResponse {
-            status: "Failed".to_owned(),
-            data: "Failed to create new user!".to_owned(),
-        }),
+    if res.is_err() {
+        return_response("Failed", "Failed to create new user")
+    } else {
+        return_response("Success", "New user was successfully created!")
     }
 }
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,25 +1,31 @@
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 #[derive(Serialize, Deserialize)]
 pub struct User {
     id: u32,
-    username: String,
-    // email: String,
+    email: String,
     password: String,
     name: String,
     // address: String,
     // phone_number: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Validate)]
 pub struct CreateUser {
-    pub username: String,
+    #[validate(email)]
+    pub email: String,
     pub password: String,
     pub name: String,
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct CreateUserResponseData {
+    pub message: String,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct CreateUserResponse {
     pub status: String,
-    pub data: String,
+    pub data: CreateUserResponseData,
 }
