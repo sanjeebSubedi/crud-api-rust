932d85c20613fc045b7ac04da790878c41a22870
Sanjeeb Subedi <spur@pop-os.localdomain>
2023-06-08 10:21:14 +0545

    move db connection to main

--- src/database/connect.rs
+++ src/database/connect.rs
@@ -1,11 +1,1 @@
-use std::error::Error;
 
-use sqlx::Pool;
-// use sqlx::Row;
-
-pub async fn connect() -> Result<Pool<sqlx::Postgres>, Box<dyn Error>> {
-    let pool =
-        sqlx::postgres::PgPool::connect("postgres://librarian:librarian@localhost:5432/library")
-            .await?;
-    Ok(pool)
-}
