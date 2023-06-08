7c01ecf1d8e7353e760a25090c76d5b6cd3a26cc
Sanjeeb Subedi <spur@pop-os.localdomain>
2023-06-08 10:22:23 +0545

    make email unique (email = username)

--- migrations/001_users_table.sql
+++ migrations/001_users_table.sql
@@ -1,6 +1,6 @@
 create table users (
     id bigserial primary key,
-    username varchar(255) not null,
+    email varchar(255) unique not null,
     password varchar(255) not null,
     name varchar(255) not null
 )
