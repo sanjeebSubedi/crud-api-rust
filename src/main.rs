5e954bc46ff0f4ed242a787a549f2c7b5e0f0710
Sanjeeb Subedi <spur@pop-os.localdomain>
2023-06-12 15:41:44 +0545

    add authentication middleware

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 mod database;
 mod handlers;
+mod middleware;
 mod models;
 mod schemas;
 use axum::{
+    middleware::from_fn,
     routing::{get, post},
     Extension, Router,
 };
 use dotenv::dotenv;
 use handlers::{
     book::{create_book, get_all_books, get_books_by_user},
+    token::get_token,
     user::{create_user, user_login},
 };
+use middleware::authenticate;
 use std::env;
 use std::error::Error;
 
@@ -21,11 +25,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let db = sqlx::postgres::PgPool::connect(&database_url).await?;
     sqlx::migrate!("./migrations").run(&db).await?;
     let app: Router = Router::new()
-        .route("/books", get(get_all_books))
-        .route("/books", post(create_book))
-        .route("/users", post(create_user))
         .route("/users/books", get(get_books_by_user))
+        .route("/books", post(create_book))
+        .layer(from_fn(authenticate::authenticate))
         .route("/login", get(user_login))
+        .route("/token", get(get_token))
+        .route("/users", post(create_user))
+        .route("/books", get(get_all_books))
         .layer(Extension(db));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
